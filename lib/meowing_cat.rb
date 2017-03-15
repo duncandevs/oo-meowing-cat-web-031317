7d855663472fdcc3b14a73f9e5a38b1f94e02889
Duncan Maina <duncandevs@gmail.com>
2017-03-15 03:37:00 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
-## code your solution here. 
+## code your solution here.
+# define class Cat
+# should be able to read and write cat name
+# method clled meow should output "meow!" using puts
+# utilize attr_accessor macros
+
+class Cat
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+end
